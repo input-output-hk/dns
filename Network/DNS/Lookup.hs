75d9d18104239300bf243d7f56bbc06adb815cb2
Alfredo Di Napoli <alfredo.dinapoli@iohk.io>
2017-08-30 11:52:14 +0200

    Fix doctest

--- Network/DNS/Lookup.hs
+++ Network/DNS/Lookup.hs
@@ -37,16 +37,17 @@
 --   >>> withResolver rs $ \resolver -> lookupA resolver hostname
 --   Right [93.184.216.34]
 --
---   The only error that we can easily cause is a timeout. We do this
+--   The only error that we can easily cause is a timeout or exhausting the
+--   number of retries we set in the resolver. We do this
 --   by creating and utilizing a 'ResolvConf' which has a timeout of
---   one millisecond:
+--   one millisecond and only 1 retry:
 --
 --   >>> let hostname = Data.ByteString.Char8.pack "www.example.com"
---   >>> let badrc = defaultResolvConf { resolvTimeout = 1 }
+--   >>> let badrc = defaultResolvConf { resolvTimeout = 1, resolvRetry = 1 }
 --   >>>
 --   >>> rs <- makeResolvSeed badrc
 --   >>> withResolver rs $ \resolver -> lookupA resolver hostname
---   Left TimeoutExpired
+--   Left RetryLimitReached
 --
 --   As is the convention, successful results will always be wrapped
 --   in a 'Right', while errors will be wrapped in a 'Left'.
@@ -183,7 +184,7 @@ lookupMX rlv dom = do
 --   >>> rs <- makeResolvSeed defaultResolvConf
 --   >>> ips <- withResolver rs $ \resolver -> lookupAviaMX resolver hostname
 --   >>> fmap sort ips
---   Right [133.138.10.34,203.178.136.49]
+--   Right [133.138.10.39,203.178.136.30]
 --
 --   Since there is more than one result, it is necessary to sort the
 --   list in order to check for equality.
